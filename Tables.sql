b0f8131ba9c0c5a28ce4fa33705d20ce72e53990
MonsieurOenologue <thomas.lfbvr@gmail.com>
2015-12-01 23:02:22 +0100

    Modifs BDD
    Suppression de TABLES et ajouts d'attributs.

--- Tables.sql
+++ Tables.sql
@@ -4,11 +4,16 @@
 --DROP TABLE IF EXISTS "UTILITAIRE" CASCADE;
 --DROP TABLE IF EXISTS "EMPLOYE" CASCADE;
 --DROP TABLE IF EXISTS "LOCATION" CASCADE;
---DROP TABLE IF EXISTS "LIAISON" CASCADE;
---DROP TABLE IF EXISTS "EMPRUNTER" CASCADE;
 --DROP TABLE IF EXISTS "MODELE" CASCADE;
---DROP TABLE IF EXISTS "LOUER" CASCADE;
 --DROP TABLE IF EXISTS "POSSEDER" CASCADE;
+
+CREATE TABLE "CLIENT"
+(
+	numero integer PRIMARY KEY,
+	nom char(30),
+	adresse char(30),
+	type_Client integer
+);
 
 CREATE TABLE "VEHICULE"
 (
@@ -17,7 +22,8 @@ CREATE TABLE "VEHICULE"
 	kilometrage integer,
 	modèle char(30),
 	marque char(30),
-	caution int
+	caution int,
+	num_Client integer REFERENCES "CLIENT"
 );
 
 CREATE TABLE "AGENCE"
@@ -27,26 +33,13 @@ CREATE TABLE "AGENCE"
 	adresse char(30)
 );
 
-CREATE TABLE "CLIENT"
-(
-	numero integer PRIMARY KEY,
-	nom char(30),
-	adresse char(30)
-);
-
-CREATE TABLE "LIAISON"
-(
-	id_Agence integer REFERENCES "AGENCE",
-	num_Client integer REFERENCES "CLIENT"
-);
-ALTER TABLE "LIAISON" ADD PRIMARY KEY (id_Agence, num_Client);
-
 CREATE TABLE "EMPLOYE"
 (
 	numero integer PRIMARY KEY,
 	nom char(30),
 	adresse char(30),
 	date_Embauche date,
+	type_Employe char,
 	est_Responsable boolean,
 	id_Agence integer REFERENCES "AGENCE"
 );
@@ -58,23 +51,10 @@ CREATE TABLE "LOCATION"
 	tarif_jour integer,
 	kilometrage_location integer,
 	nb_Jours integer,
-	num_Im integer REFERENCES "VEHICULE"
-
-);
-
-CREATE TABLE "LOUER"
-(
-	id_Location integer REFERENCES "LOCATION",
+	num_Im integer REFERENCES "VEHICULE",
 	num_Client integer REFERENCES "CLIENT"
-);
-ALTER TABLE "LOUER" ADD PRIMARY KEY (id_Location, num_Client);
 
-CREATE TABLE "EMPRUNTER"
-(
-	num_Immatriculation integer REFERENCES "VEHICULE",
-	num_Client integer REFERENCES "CLIENT"
 );
-ALTER TABLE "EMPRUNTER" ADD PRIMARY KEY (num_Immatriculation, num_Client);
 
 CREATE TABLE "POSSEDER"
 (
@@ -83,16 +63,10 @@ CREATE TABLE "POSSEDER"
 );
 ALTER TABLE "POSSEDER" ADD PRIMARY KEY (num_Agence, num_Immatriculation);
 
-CREATE TABLE "MODELE"
-(
-	id_Modele integer PRIMARY KEY,
-	num_Immatriculation integer REFERENCES "VEHICULE"
-);
-
 CREATE TABLE "UTILITAIRE"
 (
 	id_Utilitaire integer PRIMARY KEY,
-	id_Mod integer REFERENCES "MODELE",
 	capacité integer,
-	charge_Max integer
+	charge_Max integer,
+	num_Client integer REFERENCES "CLIENT"
 );
